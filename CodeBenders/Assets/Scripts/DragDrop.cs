ffdee8825dd21c7c5cbfe7573b222d01c957a502
Niyati Sharma <niya11@users.noreply.github.com>
2020-09-24 22:44:48 -0700

    Combined projectile and building scene
    Need to look into angular drag and rigidbody behavior (currently rigidbody is not attached to the blocks)

---
+++ CodeBenders/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    public float gridSize = 0.5f;
+    public bool snapToGrid = true;
+    public bool smartDrag = true;
+    public bool isDraggable = true;
+    public bool isDragged = false;
+    Vector2 initialPositionMouse;
+    Vector2 initialPositionObject;
+    
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if(isDragged)
+        {
+            if(!smartDrag)
+            {
+                transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            else
+            {
+                transform.position = initialPositionObject + (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - initialPositionMouse;
+            }
+            if(snapToGrid)
+            {
+               // transform.position.x = 1;
+                transform.position = new Vector2(Mathf.RoundToInt(transform.position.x/gridSize)*gridSize, Mathf.RoundToInt(transform.position.y/gridSize)*gridSize);
+            }
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if(isDraggable && Input.GetMouseButtonDown(0))
+        {
+            if(smartDrag)
+            {
+                initialPositionMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                initialPositionObject = transform.position;
+            }
+            isDragged = true;
+        }
+       
+    }
+    public void OnMouseUp()
+    {
+        isDragged = false;
+    }
+}
