67f17ab9f6200bbd566698dcb8fe3262be9497f7
Niyati Sharma <niya11@users.noreply.github.com>
2020-09-25 20:26:30 -0700

    Building+Battle+Camera+Target
    To-Dos':
    Fix boundary for dragging and dropping

---
+++ CodeBenders/Assets/MoveCam.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCam : MonoBehaviour
+{
+
+    public Transform[] views;
+    public float transitionSpeed;
+    Transform currentView;
+    public int[] sizes;
+    int currentSize;
+    public bool buildMode = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentView = views[1];
+        currentSize = sizes[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* Manual Camera Switching*/
+         
+        if(Input.GetKeyDown(KeyCode.Alpha1)) {
+            currentView = views[0];
+            currentSize = sizes[0];
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha2)) {
+            currentView = views[1];
+            currentSize = sizes[1];
+        }
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+        Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, currentSize, Time.deltaTime * transitionSpeed);
+    }
+
+    public void EnterBuildMode()
+    {
+        if (buildMode != true)
+            buildMode = true;
+
+        currentView = views[1];
+        currentSize = sizes[1];
+
+    }
+
+    public void ExitBuildMode()
+    {
+        if (buildMode != false)
+            buildMode = false;
+
+        currentView = views[0];
+        currentSize = sizes[0];
+    }
+
+}
--- CodeBenders/Assets/Scripts/DragDrop.cs
+++ CodeBenders/Assets/Scripts/DragDrop.cs
@@ -28,11 +28,29 @@ public class DragDrop : MonoBehaviour
             }
             if(snapToGrid)
             {
-               // transform.position.x = 1;
                 transform.position = new Vector2(Mathf.RoundToInt(transform.position.x/gridSize)*gridSize, Mathf.RoundToInt(transform.position.y/gridSize)*gridSize);
             }
+           // RectTransform rt = (RectTransform)transform;
+           // float width = rt.rect.width/2;
+           // float height = rt.rect.height/2;
+           // transform.position = new Vector2(Mathf.Clamp(transform.position.x,1.5f,12.5f),Mathf.Clamp(transform.position.y,3.0f,6.5f));
+          /*  Vector2 myPosition = transform.position;
+            Vector2 boundary = new Vector2(7, 3);
+            myPosition.x = Mathf.Clamp(myPosition.x, boundary.x, boundary.x * -1);
+            myPosition.y = Mathf.Clamp(myPosition.y, boundary.y, boundary.y * -1);
+            transform.position = myPosition;
+            */
+                 if (transform.position.x<1)
+                     transform.position = new Vector2(1.0f, Mathf.RoundToInt(transform.position.y));
+                 if (transform.position.y < 2)
+                     transform.position = new Vector2(Mathf.RoundToInt(transform.position.x), 2.0f);
+                 if (transform.position.y > 6)
+                     transform.position = new Vector2(Mathf.RoundToInt(transform.position.x), 6.0f);
+                 if (transform.position.x > 12)
+                     transform.position = new Vector2(12.0f, Mathf.RoundToInt(transform.position.y));
         }
     }
+
 
     private void OnMouseOver()
     {
