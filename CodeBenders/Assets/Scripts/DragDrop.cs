6971f2b31b2d5881411ae93723701a7b2e6104d9
Tejas Bharath <tbharath@usc.edu>
2020-09-27 16:02:24 -0400

    remove comments

--- CodeBenders/Assets/Scripts/DragDrop.cs
+++ CodeBenders/Assets/Scripts/DragDrop.cs
@@ -32,24 +32,14 @@ public class DragDrop : MonoBehaviour
             {
                 transform.position = new Vector2(Mathf.RoundToInt(transform.position.x/gridSize)*gridSize, Mathf.RoundToInt(transform.position.y/gridSize)*gridSize);
             }
-           // RectTransform rt = (RectTransform)transform;
-           // float width = rt.rect.width/2;
-           // float height = rt.rect.height/2;
-           // transform.position = new Vector2(Mathf.Clamp(transform.position.x,1.5f,12.5f),Mathf.Clamp(transform.position.y,3.0f,6.5f));
-          /*  Vector2 myPosition = transform.position;
-            Vector2 boundary = new Vector2(7, 3);
-            myPosition.x = Mathf.Clamp(myPosition.x, boundary.x, boundary.x * -1);
-            myPosition.y = Mathf.Clamp(myPosition.y, boundary.y, boundary.y * -1);
-            transform.position = myPosition;
-            */
-                 if ((transform.position.x - tileWidth/2)<1)
-                     transform.position = new Vector2(1.0f+tileWidth/2, Mathf.RoundToInt(transform.position.y));
-                 if ((transform.position.y - tileHeight/2) < 2)
-                     transform.position = new Vector2(Mathf.RoundToInt(transform.position.x), 2.0f+tileHeight/2);
-                 if ((transform.position.y+tileHeight/2) > 8)
-                     transform.position = new Vector2(Mathf.RoundToInt(transform.position.x), 8.0f-tileHeight/2);
-                 if ((transform.position.x+tileWidth/2) > 13)
-                     transform.position = new Vector2(13.0f-tileWidth/2, Mathf.RoundToInt(transform.position.y));
+            if ((transform.position.x - tileWidth/2)<1)
+              transform.position = new Vector2(1.0f+tileWidth/2, Mathf.RoundToInt(transform.position.y));
+            if ((transform.position.y - tileHeight/2) < 2)
+              transform.position = new Vector2(Mathf.RoundToInt(transform.position.x), 2.0f+tileHeight/2);
+            if ((transform.position.y+tileHeight/2) > 8)
+              transform.position = new Vector2(Mathf.RoundToInt(transform.position.x), 8.0f-tileHeight/2);
+            if ((transform.position.x+tileWidth/2) > 13)
+              transform.position = new Vector2(13.0f-tileWidth/2, Mathf.RoundToInt(transform.position.y));
         }
     }
 
