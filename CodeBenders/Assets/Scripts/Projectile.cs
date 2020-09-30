f59c6504df4e07c770fc0aeed6d9f493d5620dfa
Sijie (Tom) Bu <tombu@tombu.info>
2020-09-29 23:24:17 -0700

    Basic slingshot loader component and projectile bugfixes. - Attaches to the slingshot itself (aka the hook) - Loads the slingshot by passing the projectile game object to the LoadProjectile() method - For more details, see in-line documentation and comments - Added missing self-destruct logic to the projectile after being fired - Auomatically unloads the projectile itself from the slingshot after being fired - Currently not fully tested, since the game scene has only one projectile for firing

--- CodeBenders/Assets/Scripts/Projectile.cs
+++ CodeBenders/Assets/Scripts/Projectile.cs
@@ -6,24 +6,26 @@ using UnityEngine;
 /// </summary>
 public class Projectile : MonoBehaviour
 {
-    // internal constant: the delay after release to disable SpringJoint2D component
-    private const float SpringJointReleaseDelay = 0.15f;
-    // internal constant: the delay after disabling SpringJoin2D component for actions like self-destruction
-    private const float SpringJointPostDisableDelay = 6f;
-    // internal constant: how far is the player allowed to drag the projectile
+    // class-specific constant: how far is the player allowed to drag the projectile
     private const float MaxDragDistance = 3f;
+    // class-specific constant: the delay after disabling SpringJoin2D component for actions like self-destruction
+    private const float SpringJointPostDisableDelay = 6f;
+    // class-specific constant: the delay after release to disable SpringJoint2D component
+    private const float SpringJointReleaseDelay = 0.15f;
 
-    /// <summary>
-    /// The rigidbody object of the slingshot.
-    /// </summary>
-    public Rigidbody2D slingshot;
-    
     // whether the projectile is being dragged by the user
     private bool _beingDragged;
+    // the main camera
+    private Camera _mainCamera;
     // the rigidbody component of the projectile itself
     private Rigidbody2D _projectileBody;
     // the spring joint of the projectile itself
     private SpringJoint2D _projectileSpringJoint;
+    
+    /// <summary>
+    /// The rigidbody component of the slingshot.
+    /// </summary>
+    public Rigidbody2D slingshot;
 
     // coroutine to simulate actual slingshot firing of the projectile
     private IEnumerator FireCoroutine()
@@ -31,9 +33,12 @@ public class Projectile : MonoBehaviour
         // disable the SpringJoint2D component after some delay to allow the projectile to fly freely
         yield return new WaitForSeconds(SpringJointReleaseDelay);
         _projectileSpringJoint.enabled = false;
-        
+        // indicate that the current projectile is no longer loaded
+        slingshot.gameObject.GetComponent<SlingshotLoader>().UnloadProjectileAfterFire();
+
         // wait before further actions such as self-destruction
         yield return new WaitForSeconds(SpringJointPostDisableDelay);
+        Destroy(gameObject);
     }
     
     // event trigger for mouse click (left button down, also touch events on phones)
@@ -58,18 +63,19 @@ public class Projectile : MonoBehaviour
     private void Start()
     {
         _beingDragged = false;
-        _projectileSpringJoint = GetComponent<SpringJoint2D>();
+        _mainCamera = Camera.main;
         _projectileBody = GetComponent<Rigidbody2D>();
+        _projectileSpringJoint = GetComponent<SpringJoint2D>();
     }
 
     // called once per frame
     private void Update()
     {
-        // short circuit when the player is not dragging the projectile
-        if (!_beingDragged || Camera.main == null) return;
+        // short circuit when the player is not or unable of dragging the projectile
+        if (!_beingDragged || !_mainCamera) return;
         
         // update the projectile location to reflect it being dragged
-        var mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var mousePosition = (Vector2)_mainCamera.ScreenToWorldPoint(Input.mousePosition);
         var slingshotPosition = slingshot.position;
         if (Vector2.Distance(mousePosition, slingshotPosition) > MaxDragDistance)
             _projectileBody.position = slingshotPosition +
---
+++ CodeBenders/Assets/Scripts/SlingshotLoader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Allows simple reloading of the slingshot with new projectiles.
+/// </summary>
+public class SlingshotLoader : MonoBehaviour
+{
+    // class-specific constant: the name of the projectile that is initially loaded to the slingshot
+    private const string InitialProjectileName = "Projectile";
+    // class-specific constant: the break force of the spring joint
+    private const float SpringJointBreakForce = Single.PositiveInfinity;
+    // class-specific constant: the damping ratio of the spring joint
+    private const float SpringJointDampingRatio = 0f;
+    // class-specific constant: the distance of the spring joint
+    private const float SpringJointDistance = 0.005f;
+    // class-specific constant: the frequency of the spring joint
+    private const float SpringJointFrequency = 1.5f;
+
+    // the projectile that is being currently loaded
+    private GameObject _projectile;
+    // the rigidbody component of the slingshot itself
+    private Rigidbody2D _slingshotBody;
+
+    // called before the first frame update
+    private void Start()
+    {
+        // assumes that initially the slingshot is loaded with a projectile
+        _projectile = GameObject.Find(InitialProjectileName) ?
+            GameObject.Find(InitialProjectileName) :
+            throw new UnityException("Initial projectile not found!");
+        _slingshotBody = GetComponent<Rigidbody2D>();
+    }
+    
+    /// <summary>
+    /// Load the slingshot with a new projectile.
+    /// </summary>
+    /// <param name="projectile">The projectile to be loaded (w/o the spring joint component).</param>
+    public void LoadProjectile(GameObject projectile)
+    {
+        // update the internal reference
+        _projectile = projectile ? projectile : throw new ArgumentNullException(nameof(projectile));
+        // position the projectile
+        _projectile.transform.position = transform.position;
+
+        // create the spring joint component for the slingshot
+        var slingshotSpringJoint = _projectile.AddComponent<SpringJoint2D>();
+        slingshotSpringJoint.connectedBody = _slingshotBody;
+        slingshotSpringJoint.distance = SpringJointDistance;
+        slingshotSpringJoint.frequency = SpringJointFrequency;
+        slingshotSpringJoint.dampingRatio = SpringJointDampingRatio;
+        slingshotSpringJoint.breakForce = SpringJointBreakForce;
+    }
+
+    /// <summary>
+    /// Update the internal projectile reference after being fired.
+    /// </summary>
+    public void UnloadProjectileAfterFire()
+    {
+        _projectile = null;
+    }
+}
