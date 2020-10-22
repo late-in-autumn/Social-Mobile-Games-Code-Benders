78e9fd57b439e14dc697e249db2e21795f3bc24d
adink94 <ak59457@usc.edu>
2020-10-22 15:07:18 -0600

    Update DisplayPlayer.cs
    The Player's Turn Display Text Lingered at the Center of the Screen. Added Cross Fade Transitions to make it appear better

--- CodeBenders/Assets/Scripts/DisplayPlayer.cs
+++ CodeBenders/Assets/Scripts/DisplayPlayer.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,8 +15,8 @@ public class DisplayPlayer : MonoBehaviour
     {
         show = GameObject.FindGameObjectWithTag("PlayerTurn");
         show.SetActive(false);
-        player1 = GameObject.FindWithTag("pl1").GetComponent<InputField>().text + "'s turn";
-        player2 = GameObject.FindWithTag("pl2").GetComponent<InputField>().text + "'s turn";
+        player1 = GameObject.FindWithTag("pl1").GetComponent<InputField>().text + "'s Turn!";
+        player2 = GameObject.FindWithTag("pl2").GetComponent<InputField>().text + "'s Turn!";
     }
 
     // Update is called once per frame
@@ -31,12 +33,47 @@ public class DisplayPlayer : MonoBehaviour
         if (battle.GetComponentInChildren<Text>().text == "Start Battle!") {
             show.SetActive(true);
         }
-
+       
         show.GetComponent<Text>().text = player1;
-            
+        FadeInAndOut();
+
     }
 
-    public void changePlayer(GameObject projectile) {
-        GameObject.FindWithTag("PlayerTurn").GetComponent<Text>().text = (projectile.tag == "ProjectileP1") ? player2 : player1;        
+    public void changePlayer(GameObject projectile)
+    {
+        GameObject.FindWithTag("PlayerTurn").GetComponent<Text>().text = (projectile.tag == "ProjectileP1") ? player2 : player1;
+        FadeInAndOut();
     }
+
+    public void FadeInAndOut()
+    {
+        StartCoroutine(FadeTextAlpha());
+    }
+
+    public IEnumerator FadeTextAlpha()
+    {
+        Text i = show.GetComponent<Text>();
+        float fadeInTime  = 1f;
+        float fadeOutTime = 3f;
+
+        // First let the Text Fade In over Specified Fade-In Time
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / fadeInTime));
+            yield return new WaitForEndOfFrame();
+        }
+
+        // Next, Wait for the Frame to Change before Proceeding to Fade Out
+        yield return new WaitForEndOfFrame();
+
+        // Finally let the Text Fade Out over Specified Fade-Out Time
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / fadeOutTime));
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
 }
