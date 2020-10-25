cd4d3f3b90d86d41fc18fa7c1d24abf2dadc2f26
Astha Sharma <a@astha.me>
2020-10-25 01:11:38 -0400

    Resized all the elements for mobile

--- CodeBenders/Assets/Scripts/ProjectileGenerator.cs
+++ CodeBenders/Assets/Scripts/ProjectileGenerator.cs
@@ -10,52 +10,52 @@ public class ProjectileGenerator : MonoBehaviour
     /// Whether we allow reloading.
     /// </summary>
     public bool allowReloading;
-    
+
     /// <summary>
     /// Whether a new projectile is needed.
     /// </summary>
     public bool newProjectileNeeded;
-    
+
     /// <summary>
     /// The tag that indicates which player the projectile belongs to.
     /// </summary>
     public string projectileTag;
-    
+
     /// <summary>
     /// The sprite for the generated projectile.
     /// </summary>
     public Sprite projectileSprite;
-    
+
     /// <summary>
     /// The sorting order of the generated projectile.
     /// </summary>
     public int projectileSortingOrder;
-    
+
     /// <summary>
     /// The collider radius of the generated projectile.
     /// </summary>
     public float projectColliderRadius;
-    
+
     /// <summary>
     /// The mass of the generated projectile.
     /// </summary>
     public float projectileMass;
-    
+
     /// <summary>
     /// The linear drag of the generated projectile.
     /// </summary>
     public float projectileLinearDrag;
-    
+
     /// <summary>
     /// The angular drag of the generated projectile.
     /// </summary>
     public float projectileAngularDrag;
-    
+
     /// <summary>
     /// The gravity scale of the generated projectile.
     /// </summary>
     public float projectileGravityScale;
-    
+
     // the slingshot loader component
     private SlingshotLoader _slingshotLoader;
 
@@ -79,13 +79,15 @@ public class ProjectileGenerator : MonoBehaviour
         float gravityScale)
     {
         // include a GUID in the name so no name collision could be possible
-        var projectile = new GameObject($"Projectile ({Guid.NewGuid()})", 
+        var projectile = new GameObject($"Projectile ({Guid.NewGuid()})",
             new []{typeof(CircleCollider2D), typeof(SpriteRenderer), typeof(Rigidbody2D)});
         // tag the projectile
         projectile.tag = projectileTag;
+        //scale the projectile
+        projectile.transform.localScale = new Vector3(2, 2, 2);
         // place the projectile in the same hierarchy as the slingshot
         projectile.transform.parent = transform.parent;
-        
+
         // set up the parameters of the projectile
         projectile.GetComponent<SpriteRenderer>().sprite = sprite;
         projectile.GetComponent<SpriteRenderer>().sortingOrder = sortingOrder;
@@ -98,7 +100,7 @@ public class ProjectileGenerator : MonoBehaviour
 
         return projectile;
     }
-    
+
     //called before the first frame update
     private void Start() => _slingshotLoader = GetComponent<SlingshotLoader>();
 
@@ -106,7 +108,7 @@ public class ProjectileGenerator : MonoBehaviour
     private void Update()
     {
         if (!allowReloading || !newProjectileNeeded) return; // short circuit for not needing a new projectile
-        
+
         // currently this script handles invocation and reloading, but this should be moved out to somewhere else
         _slingshotLoader.LoadProjectile(
             GenerateProjectile(
