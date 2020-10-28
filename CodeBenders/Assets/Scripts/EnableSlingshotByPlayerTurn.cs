4228fc27fc33ced68e7d172fa6240528ec1fb679
Sijie Bu <tombu@tombu.info>
2020-10-28 00:15:27 -0700

    Enhancements to the Slingshots and Projectiles: - The slingshot will no longer auto-load immediately after firing, instead, it will only be reloaded the at the next turn of the given player (i.e. Player 1's slingshot will not be loaded during Player 2's turn)   - This should also eliminate the problem of the projectile from one player hitting the to-be-fired projectile of another player and causing both to bounce, but this effect can still be implemented if desired - The scale factor of the projectiles is now a configurable field in the ProjectileGenerator component, which potentially allows projectiles of different sizes to be easily configured

---
+++ CodeBenders/Assets/Scripts/EnableSlingshotByPlayerTurn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Enable the slingshot for a single player in a given turn.
+/// </summary>
+public class EnableSlingshotByPlayerTurn : MonoBehaviour
+{
+    /// <summary>
+    /// Enable the slingshot for a given player.
+    /// </summary>
+    /// <param name="player">The player to enable the slingshot for.</param>
+    public void EnableSlingshotForPlayer(PlayersEnum player)
+    {
+        switch (player)
+        {
+            case PlayersEnum.PlayerOne:
+                GameObject.FindWithTag("SlingshotP1").SendMessage("EnableSlingshotReloading");
+                break;
+            case PlayersEnum.PlayerTwo:
+                GameObject.FindWithTag("SlingshotP2").SendMessage("EnableSlingshotReloading");
+                break;
+        }
+    }
+}
