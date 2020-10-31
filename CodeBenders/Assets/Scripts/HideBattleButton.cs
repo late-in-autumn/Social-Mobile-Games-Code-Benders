12c27a72d7c37ca097bb8f8fe5636f1c657994e0
Astha Sharma <a@astha.me>
2020-10-30 22:50:15 -0400

    Attempt to hide start battle button

---
+++ CodeBenders/Assets/Scripts/HideBattleButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideBattleButton : MonoBehaviour
+{
+    public bool buttonPress = false;
+    public GameObject Prompt;
+    public void onButtonPress() {
+        if(buttonPress == false) {
+            buttonPress = true;
+        } else if(buttonPress == true) {
+            Prompt.SetActive(false);
+        }
+    }
+}
