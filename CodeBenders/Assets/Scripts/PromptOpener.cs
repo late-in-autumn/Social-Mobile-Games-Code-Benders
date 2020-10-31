0ee8a32ff2c5da778a58f9b8732a77d5db34f7ae
Astha Sharma <a@astha.me>
2020-10-30 22:33:21 -0400

    Added prompt to proceed to build phase

---
+++ CodeBenders/Assets/Scripts/PromptOpener.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PromptOpener : MonoBehaviour
+{
+    public GameObject Prompt;
+    public void OpenPrompt () {
+        if(Prompt != null) {
+            bool isActive = Prompt.activeSelf;
+            Prompt.SetActive(!isActive);
+        }
+    }
+}
